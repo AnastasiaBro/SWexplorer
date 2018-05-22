16a5068b76b9d97df87e3646ed9f68596cae7cc8
AnastasiaBro <a-sirion@yandex.ru>
2018-05-22 08:25:46 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import styles from './App.css';
+
+
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        data: [],
+        isLoading: false,
+      }
+    }
+    componentDidMount() {
+      const xhr = new XMLHttpRequest();
+      const URL = 'https://swapi.co/api/films';
+      xhr.open('GET', URL, true);
+      xhr.send();
+      this.setState({ isLoading: true })
+  
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState !== 4) {
+          return false
+        }
+  
+        if (xhr.status !== 200) {
+          console.log(xhr.status + ': ' + xhr.statusText)
+        } else {
+          this.setState({
+            data: JSON.parse(xhr.responseText),
+            isLoading: false,
+          })
+        }
+      }
+    }
+
+    renderFilms() {
+      const { data, isLoading } = this.state
+      if (isLoading) {
+        return <img src='/i/preloader.gif' alt='загружаю...' /> // рисуем прелоадер
+      } else {
+        if (data.results !== undefined) { //проверка, что data.results загружен
+            const results = [];
+
+            for (var i = 0; i < this.state.data.results.length; i++) {
+                results[i] = this.state.data.results[i];
+            }
+
+            const listResults = results.map((results, index) =>
+                <li className = 'film__item' key={index} onClick={this.handleClick}>
+                    <img className = 'film__image' src={'/img/' + results.title.toString() + '.jpg'} alt="film"></img>
+                    <p className = 'film__item-text film__item-text--first'>{results.title}</p>
+                    <p className = 'film__item-text'>{results.release_date.substring(0,4)}</p>
+                    <p className = 'film__item-text'>{results.director}</p>
+                    <h6 className = 'visually-hidden'>{index + 1}</h6>
+                </li>
+            );
+
+            return (
+            <div className='film'>
+                <h1 className = 'film__text'>Films</h1>
+                <p className = 'film__text'>Count: {data.count}</p>
+                <div className = 'film__container'>
+                    <ul className = 'film__list'>{listResults}</ul>
+                </div>
+                <div className = 'film__logo'>
+                    <img className = 'film__logo-image' src='/img/logo-sw.jpg' alt='logo'></img>
+                </div>
+            </div>)
+        }
+      }
+    }
+
+    handleClick = (e) => {
+        if (e.target.parentNode.className === 'film__item') {
+            console.log('this is:', e.target.parentNode.querySelector('h6').innerText);
+        } else if (e.target.className === 'film__item') {
+            console.log('this is:', e.target.querySelector('h6').innerText);
+        }
+      }
+
+    render() {
+      return (
+        <div className='App'>
+          <div className='film-list'>
+            {this.renderFilms()}
+          </div>
+        </div>
+      )
+    }
+  }
+
+export default App
