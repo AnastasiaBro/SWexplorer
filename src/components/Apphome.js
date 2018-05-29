670f13225f40f23387ddb2504984c16811a470b5
Bronnikova Anastasia <bronnikova@reksoft.ru>
2018-05-29 13:53:47 +0300

    Weather

--- src/components/Apphome.js
+++ src/components/Apphome.js
@@ -7,7 +7,11 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const WEATHER = ['rain', 'snow', 'fog', 'cloud'];
+const WEATHER = ['/img/rain.gif', '/img/snow.gif', '/img/fog.gif', '/img/sunny.gif'];
+const TEMP = ['+13°C', '-10°C', '+7°C', '+30°C'];
+const ICONS = ['/img/rain-icon.png', '/img/snow-icon.png', '/img/fog-icon.png', '/img/sun-icon.png'];
+const URLS = ['https://swapi.co/api/planets','https://swapi.co/api/planets/?page=2'];
+const DESCRIPTION = ['rain', 'snow', 'wind', 'sun'];
 
 class Apphome extends React.Component {
   constructor(props) {
@@ -19,7 +23,7 @@ class Apphome extends React.Component {
   }
   componentDidMount() {
     const xhr = new XMLHttpRequest();
-    const URL = 'https://swapi.co/api/planets';
+    const URL = URLS[getRandomInt(0, 1)];
     xhr.open('GET', URL, true);
     xhr.send();
     this.setState({ isLoading: true })
@@ -48,6 +52,7 @@ class Apphome extends React.Component {
       if (data.results !== undefined) { //проверка, что data.results загружен
           const results = [];  //данные
           const number = getRandomInt(0, 9);
+          const variant = getRandomInt(0, 3);
           console.log(number);
 
           for (var i = 0; i < this.state.data.results.length; i++) {
@@ -64,11 +69,15 @@ class Apphome extends React.Component {
 
           return (
           <div className='weather__container'>
-              <h1 className = 'weather__text'>Weather</h1>
               <div className = 'weather__block'>
                 <img className = 'weather__image' src={'/img/' + results[number].name + '.jpg'}></img>
-                <img className = 'weather__gif' src={'/img/' + WEATHER[getRandomInt(0, 1)] + '.gif'}></img>
-                <p className = 'weather__name'>{results[number].name}</p>
+                <img className = 'weather__gif' src={WEATHER[variant]}></img>
+                <div className = 'weather__bottom-line'>
+                  <p className = 'weather__temp'>{TEMP[variant]}</p>
+                  <img className = 'weather__png' src={ICONS[variant]}></img>
+                  <span className = 'weather__desc'>{DESCRIPTION[variant]}</span>
+                  <span className = 'weather__name'>{results[number].name}</span>
+                </div>
               </div>
           </div>)
       }
