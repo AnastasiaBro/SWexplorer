88ecfaf87bb1bd7c02882a13b29ef0d37d0900cd
Bronnikova Anastasia <bronnikova@reksoft.ru>
2018-05-31 16:04:22 +0300

    footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
-import Apphome from './components/Apphome'
+//import Apphome from './components/Apphome'
+import Footer from './components/Footer'
 import ReactDOM from 'react-dom';
 
 render(<App/>, document.getElementById('root'));
-render(<Apphome/>, document.getElementById('root-home'));
+//render(<Apphome/>, document.getElementById('root-home'));
+render(<Footer/>, document.getElementById('footer'));
