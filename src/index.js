08c2eef0954ad1f3d530716265acf521fd1fd7b7
Bronnikova Anastasia <bronnikova@reksoft.ru>
2018-06-04 14:04:35 +0300

    Pictures

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Header from './components/Header'
+//import Header from './components/Header'
 import App from './components/App'
 import Apphome from './components/Apphome'
 import Footer from './components/Footer'
 import ReactDOM from 'react-dom';
 
-//render(<Header/>, document.getElementById('header'));
-//render(<App/>, document.getElementById('root'));
+document.querySelector('.body-style').classList.remove('bg-films');
+document.querySelectorAll('.main-nav__item')[0].classList.add('main-nav__item--active');
+document.querySelectorAll('.main-nav__link')[0].classList.add('main-nav__link--active');
+document.querySelectorAll('.main-nav__item')[1].classList.remove('main-nav__item--active');
+document.querySelectorAll('.main-nav__link')[1].classList.remove('main-nav__link--active');
+
 render(<Apphome/>, document.getElementById('root'));
 render(<Footer/>, document.getElementById('footer'));
